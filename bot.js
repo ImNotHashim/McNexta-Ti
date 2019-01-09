326de6b0fc51b88f7e0baae83b823925828a257f
So92 <41647154+ImNotHashim@users.noreply.github.com>
2019-01-09 15:11:27 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,83 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`-new | by : iHack ,`,"http://twitch.tv/S-F")
+});
+
+client.on("message", (message) => {
+    /// ALPHA CODES
+   if (message.content.startsWith(prefix +"new")) {     /// ALPHA CODES
+        const reason = message.content.split(" ").slice(1).join(" ");     /// ALPHA CODES
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "..");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });    /// ALPHA CODES
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+ 
+            let logTkt = new Discord.RichEmbed()
+            .setTitle('OPEND A NEW TICKET')
+            .setColor('GREEN')
+            .setDescription(`tickty name: ${c.name}\nby: ${message.author.tag}`)
+            .setTimestamp()
+            .setFooter(message.author.tag, message.author.avatarURL)
+           
+            message.guild.channels.find(c => c.name === 'log-ticket').send(logTkt);
+            const embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username,message.author.avatarURL)                        
+           
+            .setColor(0xCF40FA)
+   .addField(`**مرحبآ, لدنيا فريق الدعم ليساعدك في أقرب وقت . ممكن  الرجاء منك الانتضار ريثما ياتي اليك احد اعضاء فريق الدعم**`)
+ .setTimestamp();
+              c.send({
+               
+            embed: embed
+            });
+        }).catch(console.error);
+    }
+ 
+  if (message.content.startsWith(prefix +"close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+     message.channel.send(`هل انت متأكد من اغلاق التيكت!\nلأغلاق التسكت اكتب \`$confirm\`. معاك 10 ثواني لاغلاق التيكت.`)
+         .then((m) => {
+             message.channel.awaitMessages(response => response.content === '$confirm', {
+                     max: 1,
+                     time: 10000,
+                     errors: ['time'],
+                 })    /// ALPHA CODES
+                 .then((collected) => {
+                      let logTkt = new Discord.RichEmbed()
+                      .setTitle('CLOSE TICKT')
+                      .setColor('RED')
+                      .setDescription(`tickt name: ${message.channel.name}\nby: ${message.author.tag}`)
+                      .setTimestamp()
+                      .setFooter(message.author.tag, message.author.avatarURL)
+                     
+                      message.guild.channels.find(c => c.name === 'log-ticket').send(logTkt);
+                      message.channel.delete();
+                 })    /// ALPHA CODES
+                 .catch(() => {
+                     m.edit('انتهى الوقق,  لم يتم اغلاق التذكرة الرجاء المحاولة مرة اخرى .').then(m2 => {
+                         m2.delete();
+                     }, 3000);
+                 });
+         });
+ }
+})
+
+client.login(process.env.BOT_TOKEN);
